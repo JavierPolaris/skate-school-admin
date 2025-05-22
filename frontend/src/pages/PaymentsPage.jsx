b76b93226fbb02c0510181c8e7eb880a77fbb1c0
JavierPolaris <javierrojocanton@gmail.com>
2025-05-22 09:26:20 +0200

    api/payments/notify

--- frontend/src/pages/PaymentsPage.jsx
+++ frontend/src/pages/PaymentsPage.jsx
@@ -58,7 +58,7 @@ function PaymentsPage() {
       })
     )
       .then(() => {
-        return fetch(`${API_URL}/api/payments/notify`, {
+        return fetch(`${API_URL}/payments/notify`, {
           method: 'POST',
           headers: { 'Content-Type': 'application/json' },
           body: JSON.stringify({ notificationMessage }),
