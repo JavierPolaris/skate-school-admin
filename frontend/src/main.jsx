56a0a7d107abb643d12194bb2bef6d72f8c272a4
JavierPolaris <javierrojocanton@gmail.com>
2025-05-23 08:58:31 +0200

    modifico main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,39 +4,20 @@ import './index.css'
 import App from './App.jsx'
 import { SidebarProvider } from './context/SidebarContext.jsx'
 import { AuthProvider } from './context/AuthContext';
-import { messaging, getToken, onMessage } from './firebase';
+import { requestPermissionAndGetToken, listenToForegroundMessages } from './firebase';
 
 if ('Notification' in window && navigator.serviceWorker) {
-  Notification.requestPermission().then(permission => {
-    if (permission === 'granted') {
-      getToken(messaging, { vapidKey: 'BDhxw1hQ98JM0CZFcBNuLf1Lg-l5BtBHHnybjK4zf77B6Yn6yYV6PV70O8_9nFWtSf4mRA86_crubkx5bpiZEA8' }) // la sacas de Firebase settings
-        .then(currentToken => {
-          if (currentToken) {
-            console.log('🔐 Token FCM:', currentToken);
-            fetch('https://skate-school-admin.vercel.app/api/users/save-device-token', {
-              method: 'PUT',
-              headers: {
-                'Content-Type': 'application/json',
-              },
-              body: JSON.stringify({
-                email: JSON.parse(localStorage.getItem('user') || '{}').email,
-                deviceToken: currentToken,
-              }),
-            });
-
-          } else {
-            console.warn('⚠️ No se pudo obtener el token');
-          }
-        })
-        .catch(err => console.error('❌ Error al obtener token FCM:', err));
+  requestPermissionAndGetToken().then(token => {
+    if (token) {
+      console.log('🔐 Token FCM:', token);
     }
   });
 
-  onMessage(messaging, payload => {
+  listenToForegroundMessages(payload => {
     console.log('📩 Mensaje recibido en foreground:', payload);
-    // Opcional: muestra una notificación o toast
   });
 }
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
