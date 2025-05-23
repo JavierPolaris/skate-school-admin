f6abbe604d798c58f824e1ef3da0269fde3d853c
JavierPolaris <javierrojocanton@gmail.com>
2025-05-23 10:12:09 +0200

    tokend al bakend

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -12,6 +12,22 @@ if ('Notification' in window && navigator.serviceWorker) {
       console.log('🔐 Token FCM:', token);
     }
   });
+  // ENVÍALO A TU BACKEND
+  const email = JSON.parse(localStorage.getItem('user'))?.email;
+  if (email) {
+    fetch('http://localhost:3001/api/users/save-device-token', {
+      method: 'PUT',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ email, deviceToken: token }),
+    })
+      .then(res => res.json())
+      .then(data => console.log('✅ Token guardado en backend:', data))
+      .catch(err => console.error('❌ Error al guardar token en backend:', err));
+  }
+
+
 
   listenToForegroundMessages(payload => {
     console.log('📩 Mensaje recibido en foreground:', payload);
