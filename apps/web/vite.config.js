7f579743d79d134fccc5073520ee5c44960a63d8
JavierPolaris <javierrojocanton@gmail.com>
2025-06-17 11:48:59 +0200

    ds

--- apps/web/vite.config.js
+++ apps/web/vite.config.js
@@ -2,13 +2,12 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
 
+
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
       '@shared': path.resolve(__dirname, '../../packages/shared'),
-      '@hooks': path.resolve(__dirname, './src/hooks'),
-      '@components': path.resolve(__dirname, './src/components'),
     },
   },
 });
