6725c7289d29f7709602c20307e19bc00fab1a37
JavierPolaris <javierrojocanton@gmail.com>
2025-06-23 14:06:46 +0200

    rollup

--- apps/web/vite.config.js
+++ apps/web/vite.config.js
@@ -2,12 +2,16 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
 
-
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
       '@shared': path.resolve(__dirname, '../../packages/shared'),
+    },
+  },
+  build: {
+    rollupOptions: {
+      external: ['react-native', 'expo-constants'],
     },
   },
 });
